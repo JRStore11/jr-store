2b8ff2be79b59bb2e2fa8e2e6c8b2dc0758b90fc
JRStore11 <ivaldojuniorx@gmail.com>
2026-05-18 20:18:26 -0300

    corrige firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,21 +1,20 @@
 import { initializeApp } from 'firebase/app'
-import { getDatabase } from 'firebase/database'
 import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0JIcjka1zCRZfv3o4eoi2xMtCEi6Vs",
+  apiKey: "AIzaSyAO0JICjka1zCRZfv3o4eoi2xXMtCEi6Vs",
   authDomain: "jr-store-1461b.firebaseapp.com",
   databaseURL: "https://jr-store-1461b-default-rtdb.firebaseio.com",
   projectId: "jr-store-1461b",
   storageBucket: "jr-store-1461b.firebasestorage.app",
   messagingSenderId: "16243031965",
   appId: "1:16243031965:web:29ac1d3629eef8ee21b974"
-}
+};
 
 const app = initializeApp(firebaseConfig)
 
-export const database = getDatabase(app)
-
 export const auth = getAuth(app)
+export const db = getFirestore(app)
 
 export default app
