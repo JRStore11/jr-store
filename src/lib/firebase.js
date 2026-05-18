b1a1cd2dbabe754a977e8bb344e3e1ffc8d90c3d
JRStore11 <ivaldojuniorx@gmail.com>
2026-05-18 20:28:35 -0300

    corrige firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   authDomain: "jr-store-1461b.firebaseapp.com",
   databaseURL: "https://jr-store-1461b-default-rtdb.firebaseio.com",
   projectId: "jr-store-1461b",
-  storageBucket: "jr-store-1461b.firebasestorage.app",
+  storageBucket: "jr-store-1461b.appspot.com",
   messagingSenderId: "16243031965",
   appId: "1:16243031965:web:29ac1d3629eef8ee21b974"
 };
