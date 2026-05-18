5d84f414a169076df48daa0dca5f0f4828684884
JRStore11 <ivaldojuniorx@gmail.com>
2026-05-18 20:24:58 -0300

    firebase config correta

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   projectId: "jr-store-1461b",
   storageBucket: "jr-store-1461b.firebasestorage.app",
   messagingSenderId: "16243031965",
- appId: "1:16243031965:web:29ac1d3629eef8ee21b974"
+  appId: "1:16243031965:web:29ac1d3629eef8ee21b974"
 };
 
 const app = initializeApp(firebaseConfig)
